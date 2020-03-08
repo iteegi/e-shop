4e4f99d7843a43f8963aab9c876e893ecafc18ae
J D <jd@gmail.com>
2020-03-09 00:11:00 +0300

    added cart
    cart app start
    
    Class Cart added
    
    added a form to add goods to the basket
    
    created handlers necessary for working with a basket of goods
    
    created urls patterns
    
    added detail.html
    
    added a form to add product to cart in product_detail
    
    added a form to add goods to the basket in shop/product/detail.html
    
    added a product change to cart in cart_detail
    
    Added display of quantity changes on the cart page

---
+++ cart/cart.py
@@ -0,0 +1,69 @@
+"""Cart."""
+
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+
+
+class Cart(object):
+    """Cart object."""
+
+    def __init__(self, request):
+        """Initializ the cart object."""
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # Сохраняем в сессии пустую корзину.
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def __iter__(self):
+        """Iterate for Product."""
+        product_ids = self.cart.keys()
+        # Получаем объекты модели Product и передаем их в корзину.
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """Return the total number of items in the basket."""
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """Add the product to the basket or update its quantity."""
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        """Save product in basket."""
+        # Помечаем сессию как измененную
+        self.session.modified = True
+
+    def remove(self, product):
+        """Remove item from cart."""
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def clear(self):
+        """Empty trash."""
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+    def get_total_price(self):
+        """Return total basket value."""
+        return sum(Decimal(item['price']) * item['quantity']
+                   for item in self.cart.values())
