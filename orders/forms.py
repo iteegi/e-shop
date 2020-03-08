3516719712a9d49dff3e1799ff3d4cc1f47165c2
J D <jd@gmail.com>
2020-03-09 02:44:20 +0300

    order system created
    start orders app
    
    Created order models
    
    created migration in the database
    
    registered Order class in admin panel
    
    created a form for placing an order
    
    created added order processor
    
    added urlas pattern
    
    added a link to the order_create handler in base.html
    
    added a template that displays the goods of the basket and the form for placing an order
    
    added a template for successful ordering

---
+++ orders/forms.py
@@ -0,0 +1,15 @@
+"""Forms."""
+
+from django import forms
+from .models import Order
+
+
+class OrderCreateForm(forms.ModelForm):
+    """Order form."""
+
+    class Meta:
+        """Meta information."""
+
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address',
+                  'postal_code', 'city']
