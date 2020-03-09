51c8337efeb8b3de6b6ee160fc8bedab57a69858
J D <jd@gmail.com>
2020-03-10 00:50:39 +0300

    added pdf-score
    installed WeasyPrint
    
    added html template for generating a PDF invoice for a paid order
    
    created admin_order_pdf
    
    added pdf.css
    
    added urls pattern for pdf
    
    Added a link to the PDF file for each order from the list.

--- orders/admin.py
+++ orders/admin.py
@@ -16,6 +16,13 @@ def order_detail(obj):
         reverse('orders:admin_order_detail', args=[obj.id])))
 
 
+def order_pdf(obj):
+    """Return pdf file for order."""
+    return mark_safe('<a href="{}">PDF</a>'.format(
+        reverse('orders:admin_order_pdf', args=[obj.id])))
+order_pdf.short_description = 'Счет'
+
+
 def export_to_csv(modeladmin, request, queryset):
     """Export to CSV."""
     opts = modeladmin.model._meta
@@ -37,6 +44,8 @@ def export_to_csv(modeladmin, request, queryset):
             data_row.append(value)
         writer.writerow(data_row)
     return response
+
+
 export_to_csv.short_description = 'Export to CSV'
 
 
@@ -53,7 +62,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid', 'created',
-                    'updated', order_detail]
+                    'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
