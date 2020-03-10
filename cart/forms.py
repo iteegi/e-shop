24a9690094110a216efb8f11a73bf9d98c2be864
J D <jd@gmail.com>
2020-03-10 21:00:14 +0300

    added Russian and English languages to the project
    added localization for Order model fields
    
    added localization for the form fields CartAddProductForm and CouponApplyForm
    
    added localization to html templates
    
    fixed errors in html templates
    
    installed django-rosetta
    
    new fixed errors in html templates
    
    added language prefix to templates
    
    added the ability to change the site language

--- cart/forms.py
+++ cart/forms.py
@@ -1,5 +1,6 @@
 """Forms."""
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
@@ -8,7 +9,8 @@ class CartAddProductForm(forms.Form):
     """Form for adding goods to the basket."""
 
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
-                                      coerce=int)
+                                      coerce=int,
+                                      label=_('Quantity'))
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
--- coupons/forms.py
+++ coupons/forms.py
@@ -1,9 +1,10 @@
 """Forms."""
 
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class CouponApplyForm(forms.Form):
     """Coupon form."""
 
-    code = forms.CharField()
+    code = forms.CharField(label=_('Coupon'))
